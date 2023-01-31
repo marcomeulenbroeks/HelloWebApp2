993ffb930c4fc20f44a75ca4f53369312df033c8
marcomeulenbroeks <50397094+marcomeulenbroeks@users.noreply.github.com>
2023-01-31 19:39:27 +0100

    changed welcome message

--- main.go
+++ main.go
@@ -18,5 +18,5 @@ func main() {
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
 	log.Println("index requested")
-	fmt.Fprintf(w, "Hello from Azure / GitHub")
+	fmt.Fprintf(w, "Hello from GitHub / Docker Hub")
 }
