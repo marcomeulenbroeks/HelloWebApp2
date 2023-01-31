36cebdfa00fa7c7194ec9502adb6015e22b9ecf7
marcomeulenbroeks <50397094+marcomeulenbroeks@users.noreply.github.com>
2023-01-31 18:58:04 +0100

    Created homepage of website

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/marcomeulenbroeks/HelloWebApp2
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
+
+const addr = ":8087"
+
+func main() {
+	http.HandleFunc("/", handleIndex)
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	log.Println("index requested")
+	fmt.Fprintf(w, "Hello from Azure / GitHub")
+}
