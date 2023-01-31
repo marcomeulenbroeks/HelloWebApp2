69e8150f71dafe849d395a5998fd56b02007044d
marcomeulenbroeks <50397094+marcomeulenbroeks@users.noreply.github.com>
2023-01-31 21:12:49 +0100

    setting connection time-out https://deepsource.io/directory/analyzers/go/issues/GO-S2114

--- main.go
+++ main.go
@@ -4,13 +4,21 @@ import (
 	"fmt"
 	"net/http"
 	"log"
+	"time"
 )
 
 const addr = ":8087"
 
 func main() {
 	http.HandleFunc("/", handleIndex)
-	err := http.ListenAndServe(addr, nil)
+
+	server := &http.Server{
+        Addr: addr,
+        ReadHeaderTimeout: 3 * time.Second,
+    }
+
+    err := server.ListenAndServe()
+
 	if err != nil {
         log.Fatal("ListenAndServe: ", err)
     }
